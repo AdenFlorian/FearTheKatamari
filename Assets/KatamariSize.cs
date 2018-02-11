8b8bcb9b0c6a4606cb980d207c1e40a5c0d6fda3
David Valachovic <David.E.Valachovic@gmail.com>
2018-02-10 21:44:55 -0600

    KatamariSize take 1

---
+++ Assets/KatamariSize.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KatamariSize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("size: " + calculateSize());
+	}
+
+	float calculateSize() {
+		var greatestDistance = 0f;
+		for (int i = 0; i < transform.childCount; i++) {
+			var child = transform.GetChild(i);
+			if (getDistanceToOrigin(child) > greatestDistance) {
+				greatestDistance = getDistanceToOrigin(child);
+			}
+		}
+		return greatestDistance;
+	}
+	
+	float getDistanceToOrigin(Transform outerTransform) {
+		return (outerTransform.position - transform.position).magnitude;
+	}
+}
